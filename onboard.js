4beb2bc1cb1ad0c09aea173666fc05e7697a7740
Okiemute Gold <goldokiemute@gmail.com>
2021-04-02 19:43:45 +0100

    committing completed work

---
+++ onboard.js
@@ -0,0 +1,29 @@
+// function checkMediaQuery() {
+//   // If the inner width of the window is greater then 768px
+//   if (window.innerWidth > 768) {
+//     // Then log this message to the console
+//     console.log("Media Query Matched!");
+//   }
+// }
+// // Add a listener for when the window resizes
+// window.addEventListener("resize", checkMediaQuery);
+
+// var screen = window.matchMedia("(max-width: 768px)");
+// function myFunction(x) {
+//   if (x.matches) {
+//     $(".left-content-text").html("Welcome To HRM Portal");
+//     $(".left-content-text").css({
+//       fontSize: "20px",
+//       "margin-bottom": "16px",
+//     });
+//   } else {
+//     $(".left-content-text").html(
+//       `<div>
+//             <h2>Welcome To HRM Portal</h2>
+//             <p>Create An Account Or Login To Continue</p>
+//         </div>`
+//     );
+//   }
+// }
+// screen.addListener(myFunction);
+// myFunction(screen);
